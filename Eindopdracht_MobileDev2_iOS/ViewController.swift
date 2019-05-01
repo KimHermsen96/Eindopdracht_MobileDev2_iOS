d3a659386b9fc19761205ea613d9b5a1b83884ba
kimhermsen <kahermse@avans.nl>
2019-05-01 16:24:14 +0200

    Initial Commit

---
+++ Eindopdracht_MobileDev2_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eindopdracht_MobileDev2_iOS
+//
+//  Created by Fam Hermsen on 01/05/2019.
+//  Copyright © 2019 Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
